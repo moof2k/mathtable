1fe42ff9c471282de82c034f596012f857e33850
Robert Rose <rose@perrito2.local>
2015-11-19 19:38:34 -0800

    Generating different types of problems, problem type defined through location has

--- game.js
+++ game.js
@@ -1,17 +1,45 @@
 
+function get_random(min, max) {
+    return Math.floor(Math.random() * (max - min)) + parseInt(min);
+}
 
-function GameCntl($scope, $timeout) {
-    $scope.problems = [
-        ["1", "+ 2"],
-        ["3", "- 4"]
-    ];
+
+function GameCntl($scope) {
+    $scope.t = '+';
+    $scope.min = 0;
+    $scope.max = 5;
+    $scope.problems = [];
+
+    $scope.get_params = function() {
+        var params = window.location.hash.split("/");
+
+        if (params.length == 4) {
+            $scope.t = params[1];
+            $scope.min = params[2];
+            $scope.max = params[3];
+        }
+    }
 
     $scope.generate = function() {
-        for (var i = 0; i < 20; i++)
+        var a, b;
+
+        $scope.problems = [];
+
+        for (var i = 0; i < 24; i++)
         {
-            $scope.problems.push(["1", "+ 2"]);
+            a = get_random($scope.min, $scope.max);
+            b = get_random($scope.min, $scope.max);
+            $scope.problems.push([a, $scope.t + " " + b]);
         }
     };
+
+    $(window).on('hashchange', function() {
+        $scope.get_params();
+        $scope.generate();
+        $scope.$apply();
+    });
     
+    $scope.get_params();
     $scope.generate();
 }
+
